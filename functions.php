fe20f7f7072237ae7fdbf64eba0748f8ed9081f9
Simon Prosser <simon@pagelines.com>
2014-05-13 20:04:51 +0100

    suppress php warnings if php > 5.4

--- functions.php
+++ functions.php
@@ -8,5 +8,7 @@
 	This theme copyright (C) 2008-2010 PageLines
 
 */
-
+if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+	error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE & ~E_WARNING);
+}
 require_once(TEMPLATEPATH . "/includes/core.init.php");
